477099f82ec30d3bbaf79dd599bc938ff3862c34
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 08:35:45 +0100

    Add files via upload

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,30 @@
-
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            release_year: 1973,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        },
+        002: {
+            artist: "Dino Merlin",
+            title: "Moj je zivot Svicerska",
+            release_year: 1999,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            id: 17
+        }
+    };
+    return myMusic;
+}
+myFunction()[2];
+console.log(myFunction()[2]);
+module.exports = myFunction;
