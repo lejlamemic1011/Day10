fce92bbeab4bf79f6f7c1ab9b28e0c280a931748
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 08:34:38 +0100

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,13 @@
+function myFunction(propName) {
+    var lion = {
+        name: "Simba",
+        legs: 4,
+        tails: 1,
+        roar: "roar-roar"
+    }
+    delete lion[propName];
+    return lion;
 
+}
+myFunction("roar");
+module.exports = myFunction;
