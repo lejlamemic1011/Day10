244a8b2439ef9a1bb29894b1ceba1d10f4aa9f96
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 08:06:43 +0100

    Update index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,15 @@
+// Only change code below this line
+var myPet = {
+    species: "Dog",
+    name: "Max",
+    legs: 4,
+    friends: ["Roki", "Zak"]
+}
 
+function myFunction(myPet) {
+    return myPet;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
