ede7eb5827eca27ee27949906f67502abeac609a
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-18 08:33:57 +0100

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,12 @@
+var lion = {
+    name: "Simba",
+    legs: 4,
+    tails: 1
+}
+function myFunction(propName, propValue) {
+    lion[propName] = propValue;
+    return lion;
 
+}
+myFunction("roar", "roar-roar");
+module.exports = myFunction;
